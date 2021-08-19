23219368c7f5c8e840f42472e7a504792c47d4b7
Harrison A. Pickett, IV <38798125+harrisonapickettiv@users.noreply.github.com>
2021-08-19 12:01:16 -0500

    Build UFOTofu UI with C# MVC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,13 @@ namespace UFOTofu.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Reverse()
+        {
+            Palindrome model = new();
+            return View(model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
